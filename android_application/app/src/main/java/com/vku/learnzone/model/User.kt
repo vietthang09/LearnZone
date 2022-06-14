c4e46ed0f2eee8bdeac3ff65c2b40dda8ed6d471
_hi_vietthang <lvthang.20it1@vku.udn.vn>
2022-06-14 15:11:59 +0700

    reset repository

--- android_application/app/src/main/java/com/vku/learnzone/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.vku.learnzone.model
-
-data class User(
-    val email: String? = null,
-    val password: String? = null
-)
--- android_application/app/src/main/java/com/vku/learnzone/model/Video.kt
+++
@@ -1,3 +0,0 @@
-package com.vku.learnzone.model
-
-data class Video(val image: String, val title: String, val duration: String)
